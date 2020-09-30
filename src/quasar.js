4010259aa89add534301c7bbec3f4ff31bbf070f
thobias <thobiascontact@gmail.com>
2020-09-29 21:12:07 -0300

    first commit

---
+++ src/quasar.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+
+import './styles/quasar.sass'
+import 'quasar/dist/quasar.ie.polyfills'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: { /* not needed if importStrategy is not 'manual' */ },
+  directives: { /* not needed if importStrategy is not 'manual' */ },
+  plugins: {
+  }
+ })
